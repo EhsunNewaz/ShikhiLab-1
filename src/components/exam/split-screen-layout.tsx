1dc73899e74f9c6df9ad70fbf639d5c2e6436f87
Ehsun Newaz <ehsun365@gmail.com>
2025-07-07 17:02:42 +0000

    next?

--- src/components/exam/split-screen-layout.tsx
+++ src/components/exam/split-screen-layout.tsx
@@ -9,16 +9,16 @@ interface SplitScreenLayoutProps {
 
 export function SplitScreenLayout({ leftPanel, rightPanel }: SplitScreenLayoutProps) {
   return (
-    <div className="flex h-full w-full font-exam">
+    <div className="flex h-full w-full flex-col font-exam md:flex-row">
       {/* Left Panel (Text) */}
-      <div className="h-full w-1/2 min-w-[400px] border-r-2 border-exam-border bg-white">
+      <div className="h-1/2 w-full border-b-2 border-exam-border bg-white md:h-full md:w-1/2 md:min-w-[400px] md:border-b-0 md:border-r-2">
         <ScrollArea className="h-full">
           <div className="p-5">{leftPanel}</div>
         </ScrollArea>
       </div>
       
       {/* Right Panel (Questions) */}
-      <div className="h-full w-1/2 min-w-[400px] bg-[#fafafa]">
+      <div className="h-1/2 w-full bg-[#fafafa] md:h-full md:w-1/2 md:min-w-[400px]">
          <ScrollArea className="h-full">
             <div className="p-5">{rightPanel}</div>
          </ScrollArea>
